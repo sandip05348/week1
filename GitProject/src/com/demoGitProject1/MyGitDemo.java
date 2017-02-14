a6c1b0edb18a3f5e6d512e27b4d014f283d149e9
sandip05348 <sandymmmec05@yahoo.com>
2017-02-15 01:30:03 +0530

    Change is done !!!

---
+++ GitProject/src/com/demoGitProject1/MyGitDemo.java
@@ -0,0 +1,10 @@
+package com.demoGitProject1;
+
+public class MyGitDemo {
+
+	public static void main(String[] args) {
+		System.out.println("My First Git Project How to Commit to code");
+
+	}
+
+}
